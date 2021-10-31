806ea7592d01900628cd01f2353e8004596154b6
srichmond3000 <srichmond3000@googlemail.com>
2021-10-31 13:22:13 +0000

    Fix tag manager code

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,11 +1,45 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import * as gtag from '../../lib/gtag';
 import Header from './header';
 import Footer from './footer';
 
 export default function Layout({ children, title = 'Default title' }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div className='container'>
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          strategy='afterInteractive'
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <script
+          id='gtag-init'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <link
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
